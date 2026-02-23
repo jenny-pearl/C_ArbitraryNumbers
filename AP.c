72855e6f0ff1cfe7d900534acc140a527b3d89f8
Jenny <aalper0138@gmail.com>
2026-02-23 23:27:52 +0300

    Add files via upload

--- AP.c
+++ AP.c
@@ -29,6 +29,45 @@ typedef struct {
 
 const uint64_t BIGGEST_NUMBER_USABLE = 9999999999999999999ULL;
 
+void AP_append_decimal(AP *ap, uint64_t number, uint32_t leadingZeroCount)
+{
+	if (ap->decimal.count >= ap->decimal.capacity) {
+		if (ap->decimal.capacity == 0) ap->decimal.capacity = INITIAL_COUNT;
+		else ap->decimal.capacity *= 2;
+
+		ap->decimal.limbs = realloc(ap->decimal.limbs, sizeof(uint64_t) * ap->decimal.capacity);
+
+		memset(ap->decimal.limbs + ap->decimal.count, 0,
+			sizeof(uint64_t) * (ap->decimal.capacity - ap->decimal.count));
+	}
+
+	uint32_t digitCount = 0;
+	uint64_t digitValue = 1;
+
+	for (; digitValue <= number; digitValue *= 10, digitCount++);
+
+	for (int32_t i = 0; i < LONGEST_NUMBER - digitCount - leadingZeroCount; i++) {
+		number *= 10;
+	}
+
+
+	ap->decimal.limbs[ap->decimal.count++] = number;
+}
+
+void AP_append_whole(AP *ap, uint64_t number)
+{
+	if (ap->whole.count >= ap->whole.capacity) {
+		if (ap->whole.capacity == 0) ap->whole.capacity = INITIAL_COUNT;
+		else ap->whole.capacity *= 2;
+
+		ap->whole.limbs = realloc(ap->whole.limbs, sizeof(uint64_t) * ap->whole.capacity);
+		memset(ap->whole.limbs + ap->whole.count, 0,
+			sizeof(uint64_t) * (ap->whole.capacity - ap->whole.count));
+	}
+
+	ap->whole.limbs[ap->whole.count++] = number;
+}
+
 void AP_add_to_index_whole(AP *ap, uint32_t index, uint64_t whole)
 {
 	if (ap->whole.capacity > index) {
@@ -38,6 +77,7 @@ void AP_add_to_index_whole(AP *ap, uint32_t index, uint64_t whole)
 			ap->whole.limbs[index] -= (BIGGEST_NUMBER_USABLE - whole + 1);
 			AP_add_to_index_whole(ap, index + 1, 1);
 		}
+
 		return;
 	}
 
@@ -46,13 +86,12 @@ void AP_add_to_index_whole(AP *ap, uint32_t index, uint64_t whole)
 	uint32_t max = MAX(ap->whole.capacity * 2, index + 1);
 	max = MAX(max, INITIAL_COUNT);
 
-	ap->whole.count = index + 1;
 	ap->whole.capacity = max;
+	ap->whole.count = index + 1;
 
 	ap->whole.limbs = realloc(ap->whole.limbs, ap->whole.capacity * sizeof(uint64_t));
 
-	memset(ap->whole.limbs + oldCount, 0,
-		(ap->whole.capacity - oldCount) * sizeof(uint64_t));
+	memset(ap->whole.limbs + oldCount, 0, (ap->whole.capacity - oldCount) * sizeof(uint64_t));
 
 	ap->whole.limbs[index] = whole;
 
@@ -62,11 +101,9 @@ void AP_add_to_index_whole(AP *ap, uint32_t index, uint64_t whole)
 void AP_add_to_index_decimal(AP *ap, uint32_t index, uint64_t decimal, uint32_t leadingZeroCount)
 {
 	if (ap->decimal.capacity > index) {
-
 		if (BIGGEST_NUMBER_USABLE - ap->decimal.limbs[index] >= decimal) {
 			ap->decimal.limbs[index] += decimal;
 		} else {
-
 			ap->decimal.limbs[index] -= (BIGGEST_NUMBER_USABLE - decimal + 1);
 
 			if (index != 0) {
@@ -85,8 +122,8 @@ void AP_add_to_index_decimal(AP *ap, uint32_t index, uint64_t decimal, uint32_t 
 	uint32_t max = MAX(ap->decimal.capacity * 2, index + 1);
 	max = MAX(max, INITIAL_COUNT);
 
-	ap->decimal.count = index + 1;
 	ap->decimal.capacity = max;
+	ap->decimal.count = index + 1;
 
 	ap->decimal.limbs = realloc(ap->decimal.limbs, ap->decimal.capacity * sizeof(uint64_t));
 
@@ -123,14 +160,16 @@ AP AP_init(char *format)
 	uint64_t temp = 0;
 
 	for (; index <= dot - format; index++, digitValue *= 10) {
-		temp += (uint64_t)(*(dot - index) - 48) * digitValue;
-		if (index % LONGEST_NUMBER == 0) {
-			AP_add_to_index_whole(&result, index / LONGEST_NUMBER - 1, temp);
+		if (index != 1 && (index - 1) % LONGEST_NUMBER == 0) {
+			AP_append_whole(&result, temp);
+			temp = 0;
+			digitValue = 1;
 		}
+		temp += (uint64_t)(*(dot - index) - 48) * digitValue;
 	}
 
 	if (index % LONGEST_NUMBER != 0) {
-		AP_add_to_index_whole(&result, index / LONGEST_NUMBER, temp);
+		AP_append_whole(&result, temp);
 	}
 
 	if (*dot == '\0') {
@@ -147,35 +186,37 @@ AP AP_init(char *format)
 	temp = 0;
 	digitValue = 1;
 	uint32_t leadingZero = 0;
+	bool touchedNonZero = false;
 
 	for (; index < terminator - dot; index++, digitValue *= 10) {
-		if (*(terminator - index) == '0') {
-			leadingZero++;
-		} else {
-			leadingZero = 0;
-		}
-
-		temp += (*(terminator - index) - 48) * digitValue;
-
-		if (index % LONGEST_NUMBER == 0) {
+		if (index != 1 && (index - 1) % LONGEST_NUMBER == 0) {
 			uint32_t digitCount = 0;
 			uint64_t compare = 1;
 
 			for (; compare <= temp; compare *= 10, digitCount++);
 
-			AP_add_to_index_decimal(&result,
-				index / LONGEST_NUMBER - 1,
-				temp, leadingZero);
+			AP_append_decimal(&result, temp, leadingZero);
+
+			leadingZero = 0;
+			digitValue = 1;
+			temp = 0;
 		}
+
+		if (!touchedNonZero && *(dot + index) == '0') {
+			leadingZero++;
+		} else {
+			touchedNonZero = true;
+		}
+
+		temp *= 10;
+		temp += (*(dot + index) - 48);
 	}
 
 	if (index % LONGEST_NUMBER != 0) {
 		uint32_t digitCount = 0;
 		uint64_t compare = 1;
 		for (; compare <= temp; compare *= 10, digitCount++);
-		AP_add_to_index_decimal(&result,
-			index / LONGEST_NUMBER,
-			temp, leadingZero);
+		AP_append_decimal(&result, temp, leadingZero);
 	}
 
 	return result;
@@ -207,7 +248,7 @@ int main(void)
 {
 	AP ap = {0};
 
-	ap = AP_init("123123123123.123123123123");
+	ap = AP_init("111222333444555666777888999.000111222333444555666777888999");
 
 	AP_print(&ap);
 
